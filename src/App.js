a90f39cb4caf3e1e8c78c96401da59b2b2014fe8
Saidur Rahman <srahman@atlassian.com>
2019-02-05 09:57:05 +1100

    make subscriptions work

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import ApolloClient from "apollo-boost";
+import ApolloClient from "apollo-client";
 import { ApolloLink, concat } from 'apollo-link'
 
 import { ApolloProvider } from "react-apollo";
@@ -8,39 +8,44 @@ import Polling from "./components/Polling";
 import Refetch from "./components/Refetch";
 import Mutation from "./components/Mutation";
 import Subscription from "./components/Subscription";
-import { SubscriptionClient } from 'subscriptions-transport-ws'
-import { createHttpLink } from 'apollo-link-http'
+import { SubscriptionClient, addGraphQLSubscriptions } from 'subscriptions-transport-ws'
+import { createHttpLink, HttpLink } from 'apollo-link-http'
 import { WebSocketLink } from 'apollo-link-ws'
 import { getMainDefinition } from 'apollo-utilities';
+// import { createNetworkInterface } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+  // const client = new ApolloClient({
+  //   uri: 'http://localhost:4000/graphql',
+  //   cache: new InMemoryCache()
+  // })
+
+
+const localHTTPGraphQL = "http://localhost:4000/graphql";
+const localWSGraphQL = "ws://localhost:4000/graphql";
 
 const hasSubscriptionOperation = ({ query: { definitions } }) =>
-({ query }) => {
-  const { kind, operation } = getMainDefinition(query);
-  return kind === 'OperationDefinition' && operation === 'subscription';
-}
-
-const localGraphQL = "http://localhost:4000/graphql";
-
+  definitions.some(
+    ({ kind, operation }) =>
+      kind === 'OperationDefinition' && operation === 'subscription',
+  )
 
 const link = ApolloLink.split(
   hasSubscriptionOperation,
   new WebSocketLink(
-    new SubscriptionClient('ws://localhost:4000/graphql', {
+    new SubscriptionClient(localWSGraphQL, {
       reconnect: true,
-      keepAlive: true
     }),
   ),
   createHttpLink({
-    uri: 'http://localhost:4000/graphql',
+    uri: localHTTPGraphQL,
   }),
 )
 
-
-const client = new ApolloClient({ 
+const client = new ApolloClient({
   link,
-  uri: localGraphQL
-});
-
+  cache: new InMemoryCache(),
+})
 
 class App extends Component {
   render() {
